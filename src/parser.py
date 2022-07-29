fdf1c8c84a63ff195294f12e208d296f70679379
Joellensilva <joellen.absilva@gmail.com>
2022-07-29 11:19:44 -0300

    * (-1)

--- src/parser.py
+++ src/parser.py
@@ -6,7 +6,8 @@ import number
 
 from coleta import coleta_pb2 as Coleta
 
-from headers_keys import (MES13, REMUNERACAOBASICA, EVENTUALTEMP, OBRIGATORIOS, HEADERS)
+from headers_keys import (MES13, REMUNERACAOBASICA,
+                          EVENTUALTEMP, OBRIGATORIOS, HEADERS)
 
 
 def parse_employees(file, colect_key, file_cq13, month):
@@ -16,7 +17,8 @@ def parse_employees(file, colect_key, file_cq13, month):
         cq13 = contracheque13(file_cq13)
     for row in file:
         if not number.is_nan(row[0]):
-            registration = str(row[0])[:-2] # Precisamos disso pois o pandas entende que a matrícula é um número float.
+            # Precisamos disso pois o pandas entende que a matrícula é um número float.
+            registration = str(row[0])[:-2]
             name = row[1]
             function = row[2]
             location = row[3]
@@ -33,11 +35,11 @@ def parse_employees(file, colect_key, file_cq13, month):
                 if month == "12" and row[0] in file_cq13:
                     employee = cq13.get(registration)
                     member.remuneracoes.CopyFrom(
-                    create_contracheque(row, month, employee)
-                    ) 
-                else: 
+                        create_contracheque(row, month, employee)
+                    )
+                else:
                     member.remuneracoes.CopyFrom(
-                    create_contracheque(row, month, "")
+                        create_contracheque(row, month, "")
                     )
 
                 employees[registration] = member
@@ -45,20 +47,23 @@ def parse_employees(file, colect_key, file_cq13, month):
 
     return employees
 
+
 def remunerations(file_indenizatorias):
     dict_remuneracoes = {}
     for row in file_indenizatorias:
-        mat = str(row[0])
-        remuneracoes = dict_remuneracoes.get(mat, Coleta.Remuneracoes())
-        rem = Coleta.Remuneracao()
-        rem.natureza = Coleta.Remuneracao.Natureza.Value("R")
-        rem.categoria = str(row[4])
-        rem.item = str(row[5])
-        rem.valor = float(number.format_value(row[6]))
-        rem.tipo_receita = Coleta.Remuneracao.TipoReceita.Value("O")
-        remuneracoes.remuneracao.append(rem)
-        dict_remuneracoes[mat] = remuneracoes
+        if not number.is_nan(row[0]) and number.is_nan(row[6]):
+            mat = str(row[0])
+            remuneracoes = dict_remuneracoes.get(mat, Coleta.Remuneracoes())
+            rem = Coleta.Remuneracao()
+            rem.natureza = Coleta.Remuneracao.Natureza.Value("R")
+            rem.categoria = str(row[4])
+            rem.item = str(row[5])
+            rem.valor = float(number.format_value(row[6]))
+            rem.tipo_receita = Coleta.Remuneracao.TipoReceita.Value("O")
+            remuneracoes.remuneracao.append(rem)
+            dict_remuneracoes[mat] = remuneracoes
     return dict_remuneracoes
+
 
 def create_indenizacoes(employee, remuneracoes):
     if employee in remuneracoes.keys():
@@ -86,9 +91,10 @@ def create_contracheque(row, month, employee):
         if month == "12" and value in [7, 9] and employee != "":
             for emp in employee.remuneracao:
                 if key in emp.item:
-                    remuneration.valor = float(number.format_value(row[value])) + emp.valor
-        else: 
-            remuneration.valor = float(number.format_value(row[value])) 
+                    remuneration.valor = float(
+                        number.format_value(row[value])) + emp.valor
+        else:
+            remuneration.valor = float(number.format_value(row[value]))
         remuneration.tipo_receita = Coleta.Remuneracao.TipoReceita.Value("B")
         remuneration_array.remuneracao.append(remuneration)
 
@@ -100,19 +106,23 @@ def create_contracheque(row, month, employee):
         if month == "12" and value in [13, 14] and employee != "":
             for emp in employee.remuneracao:
                 if key in emp.item:
-                    remuneration.valor = float(number.format_value(row[value])) + emp.valor
-        else: 
-            remuneration.valor = float(number.format_value(row[value]))
+                    remuneration.valor = (
+                        float(number.format_value(row[value])) + emp.valor) * (-1)
+        else:
+            remuneration.valor = float(number.format_value(row[value]))*(-1)
         remuneration.natureza = Coleta.Remuneracao.Natureza.Value("D")
         remuneration_array.remuneracao.append(remuneration)
 
     return remuneration_array
 
+
 def contracheque13(cq13):
     dict_cq13 = {}
     for row in cq13:
-        if not number.is_nan(row[0]): # Para não confundir o cabeçalho da planilha de contracheque com os valores. 
-            mat = str(row[0])[:-2]    # Precisamos disso pois o pandas entende que a matrícula é um número float.
+        # Para não confundir o cabeçalho da planilha de contracheque com os valores.
+        if not number.is_nan(row[0]):
+            # Precisamos disso pois o pandas entende que a matrícula é um número float.
+            mat = str(row[0])[:-2]
             remuneracoes = dict_cq13.get(mat, Coleta.Remuneracoes())
             for key, value in MES13[EVENTUALTEMP].items():
                 remuneration = Coleta.Remuneracao()
@@ -120,13 +130,15 @@ def contracheque13(cq13):
                 remuneration.categoria = EVENTUALTEMP
                 remuneration.item = key
                 remuneration.valor = float(number.format_value(row[value]))
-                remuneration.tipo_receita = Coleta.Remuneracao.TipoReceita.Value("B")
+                remuneration.tipo_receita = Coleta.Remuneracao.TipoReceita.Value(
+                    "B")
                 remuneracoes.remuneracao.append(remuneration)
             for key, value in MES13[OBRIGATORIOS].items():
                 remuneration = Coleta.Remuneracao()
                 remuneration.categoria = OBRIGATORIOS
                 remuneration.item = key
-                remuneration.valor = float(number.format_value(row[value]))
+                remuneration.valor = float(
+                    number.format_value(row[value]))
                 remuneration.natureza = Coleta.Remuneracao.Natureza.Value("D")
                 remuneracoes.remuneracao.append(remuneration)
             dict_cq13[mat] = remuneracoes
@@ -138,16 +150,19 @@ def update_employees(file_indenizatorias, employees):
     for employee in employees:
         emp = employees[employee]
         remu = create_indenizacoes(employee, remuneracoes)
-        emp.remuneracoes.MergeFrom(remu)
+        if "NoneType" not in str(type(remu)):
+            emp.remuneracoes.MergeFrom(remu)
         employees[employee] = emp
-    return employees 
+    return employees
+
 
 def parse(data, colect_key):
     employees = {}
     payroll = Coleta.FolhaDePagamento()
     cq13 = contracheque13(data.contracheque13)
 
-    employees.update(parse_employees(data.contracheque, colect_key, data.contracheque13, data.month))
+    employees.update(parse_employees(data.contracheque,
+                     colect_key, data.contracheque13, data.month))
     update_employees(data.indenizatorias, employees)
 
     for i in employees.values():
